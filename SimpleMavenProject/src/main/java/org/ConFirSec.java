122e0feff85254e35c596a9722a2de861b2fcfa1
Spoorthi G <spoorthi.g@publicissapient.com>
2022-04-05 17:21:13 +0530

    maven file

---
+++ SimpleMavenProject/src/main/java/org/ConFirSec.java
@@ -0,0 +1,19 @@
+package org;
+
+import java.util.Scanner;
+
+public class ConFirSec {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter first name");
+		String a=sc.next();
+		System.out.println("enter last name");
+		String b=sc.next();
+		System.out.println("Hello " + a + " " + b);
+		
+		
+		
+	}
+
+}
